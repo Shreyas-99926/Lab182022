631a78094e0e210718ade459c0a81118fc384a08
Shreyas S <shreyas99926@gmail.com>
2022-08-05 10:41:40 +0530

    add

--- Chapter1Part1/Program.cs
+++ Chapter1Part1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Enter the value of a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the value of b");
+int b = int.Parse(Console.ReadLine());
+int sum = 0;
+sum = a + b;
+Console.WriteLine($"sum of two numbers is {sum}:");
